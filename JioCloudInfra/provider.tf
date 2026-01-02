620c86634166e675c608ca14debb59a3e198da9a
Akshay Pakade <pakadeakshay@gmail.com>
2026-01-02 18:41:13 +0530

    Add Azure backend configuration to provider.tf
    Updated Terraform configuration to include backend settings for Azure storage.

--- JioCloudInfra/provider.tf
+++ JioCloudInfra/provider.tf
@@ -5,14 +5,18 @@ terraform {
       version = "~> 3.0"
     }
   }
-terraform {
-  backend "azurerm" {}
-}
-}
+ backend "azurerm" {
+    resource_group_name  = "dev-resource-group"
+    storage_account_name = "devinfrastorage9810"
+    container_name       = "jenkins"
+    key                  = "jioinfra.tfstate"
+  }
+  }
 provider "azurerm" {
   features {}
 subscription_id = "de1c1815-4f90-412b-9551-d55f0de9407d"
 }
+
 
 
 
