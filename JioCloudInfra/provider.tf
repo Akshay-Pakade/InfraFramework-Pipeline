3de41d84b7d8d591cdd16ec949a194ef3e52b349
Akshay Pakade <pakadeakshay@gmail.com>
2026-01-02 18:28:41 +0530

    Update provider.tf

--- JioCloudInfra/provider.tf
+++ JioCloudInfra/provider.tf
@@ -5,10 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+terraform {
+  backend "azurerm" {}
+}
 }
 provider "azurerm" {
   features {}
 subscription_id = "de1c1815-4f90-412b-9551-d55f0de9407d"
 }
+
 
 
